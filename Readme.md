3cc139792a6cf176ff6ec548b6d45c0b41f2bb04
Sven Helmberger <fforw@gmx.de>
2016-08-21 22:32:47 +0200

    implement plugin

--- Readme.md
+++ Readme.md
@@ -1,1 +1,48 @@
 # Webpack symbol loader
+
+A webpack loader to extract graphical symbols from SVG documents to be used within a web context
+(e.g. React).
+
+## Symbols
+
+A symbol is basically an named array of SVG groups. To define a symbol, you need to 
+create a SVG <g/> element with the id set in a special format
+
+```HTML
+<g id="symbol:name">
+    ...
+</g>
+```
+
+This defines a symbol group with the name "name". The symbol names can be any valid id character but ":".
+You can define symbol layers by adding another colon separated identifier
+
+
+```HTML
+<g id="symbol:name:layer1">
+    ...
+</g>
+<g id="symbol:name:layer2">
+    ...
+</g>
+<g id="symbol:name:layer3">
+    ...
+</g>
+```
+
+Here, the symbol "name" consist of three different layers, to be drawn in document definition order
+as usual with SVG.
+
+The loader will keep track of transformations on that symbol groups and their parents and
+will adjust the created offsets accordingly. Translation should always work, rotation and
+matrix transforms can cause problems / have to be put at the right level to work. The current
+aproach works, but is somewhat primitive.
+
+The loader allows cleanup of namespaces from vector graphic programs like Inkscape (Advertisment: *use Inkscape, it's awesome*)
+
+
+# Links
+
+[Inkscape: open-source vector graphics editor](https://inkscape.org/)
+
+
---
+++ test/test-loader.js
@@ -0,0 +1,128 @@
+var assert = require("power-assert");
+var path = require("path");
+var sinon = require("sinon");
+var fs = require("fs");
+
+var Loader = require("../lib/loader");
+
+var moduleExportsRE = /module.exports = (.*);/;
+
+function extractJSONExport(doc)
+{
+    var m = moduleExportsRE.exec(doc);
+    if (!m)
+    {
+        throw new Error("Could not extract JSON export");
+    }
+
+    return JSON.parse(m[1]);
+}
+
+
+function load(relPath, cb)
+{
+
+    var cacheableSpy = sinon.spy();
+
+    fs.readFile(path.join(__dirname, relPath), "UTF8", function (err, data)
+    {
+        if (err)
+        {
+            throw err;
+        }
+
+        Loader.call({
+            cacheable: cacheableSpy,
+            async: function ()
+            {
+                return (err, doc) => {
+                    if (err)
+                    {
+                        cb(err);
+                    }
+
+                    // should be cacheable
+                    assert(cacheableSpy.called);
+
+                    cb(null, extractJSONExport(doc));
+                };
+            }
+        }, data)
+    })
+}
+
+describe("Symbol loader", function(){
+
+
+
+    it("loads symbols", function(done)
+    {
+        load("test-documents/test.svg", (err, data) => {
+
+            if (err)
+            {
+                done(err);
+                return;
+            }
+            console.log(JSON.stringify(data, null, 4));
+
+            assert(data.foo.name === "foo");
+            assert(data.foo.layers.length === 1);
+            assert(data.foo.layers[0] === "default");
+
+            assert(data.foo.groups.length === 1);
+            assert(data.foo.groups[0].indexOf('rect') >= 0);
+            assert(data.foo.styles[0] === "fill: #00f;");
+
+            assert.deepEqual(data.foo.center, { x: 50, y: 50});
+
+            done();
+        });
+
+    });
+    it("supports multiple layers", function(done)
+    {
+        load("test-documents/test2.svg", (err, data) => {
+
+            if (err)
+            {
+                done(err);
+                return;
+            }
+            //console.log(JSON.stringify(data, null, 4));
+
+            assert(data.bar.layers.length === 2);
+            assert(data.bar.layers[0] === "bg");
+            assert(data.bar.layers[1] === "fg");
+
+            assert(data.bar.groups.length === 2);
+            assert(data.bar.groups[0].indexOf('rect') >= 0);
+            assert(data.bar.groups[1].indexOf('circle') >= 0);
+
+            assert(data.bar.center.x|0 === 0);
+            assert(data.bar.center.y|0 === 70);
+
+            done();
+        });
+    });
+
+    it("linearizes paths", function(done)
+    {
+        load("test-documents/test3.svg", (err, data) => {
+
+            if (err)
+            {
+                done(err);
+                return;
+            }
+            console.log(JSON.stringify(data, null, 4));
+
+            assert(data.qux.layers.length === 1);
+            assert(data.qux.layers[0] === "default");
+
+            assert(data.qux.center.y > 450);
+
+            done();
+        });
+    });
+});
